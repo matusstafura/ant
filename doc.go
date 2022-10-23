454735ed5d3ba47191c1eb8221a276cc1c1acb96
matusstafura <kobe21@gmail.com>
2022-10-23 09:53:37 +0200

    docs: doc.go

---
+++ doc.go
@@ -0,0 +1,31 @@
+/*
+# Various Helpers in GoLang
+
+## Usage
+
+import (
+	"fmt"
+	"github.com/matusstafura/ant/extract"
+)
+
+func main() {
+	sitemap := extract.UrlsFromFile("content/test/sitemap.xml")
+	fmt.Println(sitemap) // [http://www.example.com http://www.other.com]
+}
+
+## Tests
+
+```shell
+go test ./... -v
+```
+
+## Contributions
+
+Contributions are welcome. Please see [CONTRIBUTING](https://github.com/matusstafura/.github/blob/main/CONTRIBUTING.md) for details.
+
+## License
+
+This project is open-sourced software licensed under the MIT license.
+*/
+
+package main
